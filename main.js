bf4af84116271d33f817a823b16ae9bc80df288c
CannelleJP <isaibmezghiche@edenschool.fr>
2022-11-29 17:17:45 +0100

    jej

--- main.js
+++ main.js
@@ -38,9 +38,26 @@ for (let i = 0; i < tablo.length; i++) {
 
     let art = document.createElement('article')
     let h1 = document.createElement('h1')
+    let para = document.createElement('p')
     art.classList.add('sect')
+   
     h1.innerHTML = tablo[i].titre
+    
+    para.innerHTML = tablo[i].subtitle
+    para.classList.add('subtitle')
+art.addEventListener('click',()=>{
+para.style.display = "flex"
+art.style.height = "200px"
+
+
+
+
+})
+
     h1.classList.add('titre')
+
     art.appendChild(h1)
+    art.appendChild(para)
     general.appendChild(art)
 }
+
