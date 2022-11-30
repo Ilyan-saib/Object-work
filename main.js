ea2d3479b3c3b691ef7cf49a8d2da5b9068b9ceb
CannelleJP <isaibmezghiche@edenschool.fr>
2022-11-30 16:36:37 +0100

    so les chiffres

--- main.js
+++ main.js
@@ -40,35 +40,59 @@ for (let i = 0; i < tablo.length; i++) {
     let h1 = document.createElement('h1')
     let para = document.createElement('p')
     art.classList.add('sect')
-   
+
     h1.innerHTML = tablo[i].titre
-    
+
     para.innerHTML = tablo[i].subtitle
     para.classList.add('subtitle')
-art.addEventListener('click',()=>{
-general.style.height = `${125 * tablo.length}px`
 
-
-if (art.style.height === "200px") {
-    para.style.display = "none"
-art.style.height = "100px"
-art.style.width = "800px"
-art.style.position = "relative"
-   art.style.right = "0px"
-general.style.height = `${100 * tablo.length}px`
-}
+    art.addEventListener('click', () => {
 
 
 
-else{
-    para.style.display = "flex"
-    art.style.height = "200px"
-   art.style.width = "900px" 
-   art.style.position = "relative"
-   art.style.right = "50px"
-}
+        general.style.height = `${125 * tablo.length}px`
+        let artcles = document.getElementsByClassName('sect')
 
-})
+        for (let j = 0; j < artcles.length; j++) {
+
+            if (artcles[j] === art) {
+           
+                continue
+
+            }
+            artcles[j].style.border = "1px solid grey"
+            artcles[j].style.width = "800px"
+            artcles[j].style.height = "100px"
+
+            artcles[j].children[1].style.display = "none"
+            general.style.height = `${125 * tablo.length}px`
+            artcles[j].style.position = "relative"
+            artcles[j].style.right = "0px"
+
+        }
+
+
+        if (art.style.height === "200px") {
+            art.classList.add('lock')
+            art.classList.remove('unlock')
+           //para.style.display = "none"
+        }
+
+
+
+        else {
+            //     art.classList.remove('lock')
+            //     art.classList.add('unlock')
+             para.style.display = "flex"
+            art.style.height = "200px"
+            art.style.width = "900px"
+            art.style.position = "relative"
+            art.style.right = "50px"
+            art.style.border = "5px solid black"
+        }
+
+    })
+
 
 
 
