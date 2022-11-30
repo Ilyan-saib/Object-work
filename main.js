70a0403c5247cab11dcd56883a390248796d1ada
CannelleJP <isaibmezghiche@edenschool.fr>
2022-11-30 11:12:36 +0100

    team anti lack

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 let tablo = [{
 
-    titre: "1. Hotel facilities",
-    subtitle: `We've got everything you need covered, from hairdryer to
-    ski storage at the lobby. Don't worry about that!`
+    titre: "1. Lorem Ipsum ?",
+    subtitle: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Nihil, quia quos quidem sequi pariatur omnis atque vel. Quasi!`
 },
 {
 
-    titre: "2. Je sais pas quoi  ",
-    subtitle: `We've got everything you need covered, from hairdryer to
-    ski storage at the lobby.`
-
-},
-
-{
-
-    titre: "3. Je sais quoi  ",
-    subtitle: `from hairdryer to
-    ski storage at the lobby.`
+    titre: "2.  Ipsum Lorem?",
+    subtitle: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Nihil, quia quos quidem sequi pariatur omnis atque vel. Quasi!`
 
 },
 
 {
 
-    titre: "4. Je sais quoi  ",
-    subtitle: `from hairdryer to
-    ski storage at the lobby.`
+    titre: "3. ¿muspI meroL",
+    subtitle: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Nihil, quia quos quidem sequi pariatur omnis atque vel. Quasi!`
+
+},
+
+{
+
+    titre: "4. L o r e m ( ﾉ ﾟｰﾟ)ﾉ i p s u m ",
+    subtitle: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Nihil, quia quos quidem sequi pariatur omnis atque vel. Quasi!`
 
 },
 ]
@@ -46,13 +46,32 @@ for (let i = 0; i < tablo.length; i++) {
     para.innerHTML = tablo[i].subtitle
     para.classList.add('subtitle')
 art.addEventListener('click',()=>{
-para.style.display = "flex"
-art.style.height = "200px"
+general.style.height = `${125 * tablo.length}px`
+
+
+if (art.style.height === "200px") {
+    para.style.display = "none"
+art.style.height = "100px"
+art.style.width = "800px"
+art.style.position = "relative"
+   art.style.right = "0px"
+general.style.height = `${100 * tablo.length}px`
+}
 
 
 
+else{
+    para.style.display = "flex"
+    art.style.height = "200px"
+   art.style.width = "900px" 
+   art.style.position = "relative"
+   art.style.right = "50px"
+}
 
 })
+
+
+
 
     h1.classList.add('titre')
 
